fb5e9694b6af013f652c1302ca5f4f44bb3ba80c
Marek Lewiński <lewinski.marek@outlook.com>
2016-10-14 21:52:35 +0200

    Implemented Chineese Primality Test

--- Number Theory/ChineesePrimalityTest.cpp
+++ Number Theory/ChineesePrimalityTest.cpp
@@ -3,7 +3,38 @@
 
 using namespace std;
 
-int main(){
+typedef unsigned long long int ulong;
 
+ulong ModuloMultiplication(ulong a, ulong b, ulong n)
+{
+    ulong m=1, w=0;
+
+    while(m)
+    {
+        if(b&m) w=(w+a)%n;
+        a=(a<<1)%n;
+        m<<=1;
+    }
+    return w;
+}
+
+ulong ModuloPower(ulong b, ulong n)
+{
+    ulong m=1,p=2,w=1;
+    while(m)
+    {
+        if(b&m) w = ModuloMultiplication(w,p,n);
+        p=ModuloMultiplication(p,p,n);
+        m<<=1;
+    }
+    return w;
+}
+
+int main()
+{
+    ulong p=0;
+    cin>>p;
+    cout<<(ModuloPower(p,p) == 2 ? "Prime":"Not prime")<<endl;
+    return 0;
 
 }
